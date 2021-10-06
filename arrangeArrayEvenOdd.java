57b11dc5b072c776ffce74bdce4c4ca9327e9711
Aditya Galande <adityagalande14@gmail.com>
2021-10-06 23:23:51 +0530

    arrangeArrayEvenOdd

--- arrangeArrayEvenOdd.java
+++ arrangeArrayEvenOdd.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
 public class arrangeArrayEvenOdd {
 
+public static void arrangeEvenOdd(int[] arr, int n){
+
+Arrays.sort(arr);
+
+for(int i=0; i<n; i++){
+System.out.println(arr[i]);
+}
+
+}
+
 public static void main(String []args){
-System.out.println("HAHAHAHAHAHAH");
+int[] arr = {9,6,8,2,1,5,4,12,17,13,20};
+int n = arr.length;
+arrangeEvenOdd(arr, n);
 }
 }
