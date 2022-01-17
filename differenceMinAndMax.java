4d580a426f10b35c4c43a216bc7350bd11d4669f
Aditya Galande <adityagalande14@gmail.com>
2022-01-17 21:39:35 +0530

    differenceMinAndMax(Prime_Number)

--- differenceMinAndMax.java
+++ differenceMinAndMax.java
@@ -1,21 +1,28 @@
 public class differenceMinAndMax{
     
     public static int differenceMinAndMax(int[] nums){
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        boolean flag = true;
+        
         if(nums.length < 1) return 0;
         
         for(int i=0; i<nums.length; i++){
-            for(int j=2; j<i; j++){
-                if(nums[i] % j == 0) break;
-                if(nums[i] > max) max = nums[i];
-                if(nums[i] < min) min = nums[i];
+            for(int j=2; j<nums[i]; j++){
+                if(nums[i] % j == 0) flag = false;
             }
+            
+            if(flag == true && nums[i] > 1){
+                if(max < nums[i]) max = nums[i];
+                if(min > nums[i]) min = nums[i];
+            }
+            flag = true;
         }
+        
         return max-min;
     }
 
      public static void main(String []args){
-         int[] nums = {19,22,13,8,7,24,14,-5,-1};
+         int[] nums = {19,22,13,8,7,24,14,-5,1};
          System.out.println(differenceMinAndMax(nums));
      }
 }
